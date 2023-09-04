0971d754d545737aba9120af4c3f185c0aadaf69
Jeromemoores <mooresj15@gmail.com>
2023-09-04 09:09:41 -0500

    - Changed Font

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import { WebsiteNavigation } from './components'
 
+import '@fontsource-variable/jetbrains-mono'
+import './style/main.css'
+
 createRoot(document.getElementById('root')).render(<WebsiteNavigation />)
