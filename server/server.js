4fff07a60dbe1a335387a06d062c66689435925d
Jeromemoores <mooresj15@gmail.com>
2023-09-04 08:54:24 -0500

    - Server initialized

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const db = require('./models')
+const { PORT, SECRET } = require('./config')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use('/*', function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With,GET,POST,content-type,Origin,Accept')
+	req.header('Access-Control-Allow-Origin', '*')
+	req.header('Access-Control-Allow-Headers', 'X-Requested-With,GET,POST,content-type,Origin,Accept')
+	next()
+})
+
+db.sequelize.sync().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Server Running on ${PORT}`)
+	})
+})
